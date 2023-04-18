abc9fe6299dbcccf660092df65e3dcca658ae08e
Alexey Lagun <alexey.lagun@gmail.com>
2023-04-19 00:50:52 +0300

    feat: add item

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: "Старый стул",
+          img: "old-chair (1).jpg",
+          desc: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam architecto porro modi, unde velit doloremque! Asperiores voluptates rem voluptas repellendus. Vero, adipisci? Maiores modi cumque fuga veritatis dolorum expedita perspiciatis.",
+          category: "chair",
+          price: "49,99",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
