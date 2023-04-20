85446fe65a4ab430560673664b676faed64559df
Alexey Lagun <alexey.lagun@gmail.com>
2023-04-20 19:22:52 +0300

    feat: add cart at the page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
   return (
     <header>
       <div>
@@ -10,6 +12,12 @@ export default function Header() {
           <li>Контакты</li>
           <li>Кабинет</li>
         </ul>
+        <FaShoppingCart
+          onClick={() => setCartOpen((cartOpen = !cartOpen))}
+          className={`shop__cart__button ${cartOpen && "active"}`}
+        />
+
+        {cartOpen && <div className="shop__cart"></div>}
       </div>
       <div className="presentation"></div>
     </header>
